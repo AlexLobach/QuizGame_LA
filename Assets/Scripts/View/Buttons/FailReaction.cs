11b9d42892739313595d9ec0724fcb65a453827b
AlexLobach <alexlobatch@yandex.ru>
2024-08-01 13:08:00 +0300

    FailReaction

---
+++ Assets/Scripts/View/Buttons/FailReaction.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using View;
+
+namespace View
+{
+    [RequireComponent(typeof(Button))]
+    public class FailReaction : MonoBehaviour, IButtonEffect
+    {
+        [SerializeField] private bool use = true;
+        private float durationEffect = 0.8f;
+        private float   durationPause = 0.3f;        
+        private Tween currentTween;
+        private Vector3 punchPosition;
+
+
+
+
+
+        void Awake()
+        {
+            punchPosition = new Vector3 (20,0,0) ;
+        }
+
+        
+        public void Notify(bool correct)
+        {
+            if (!use || correct)
+            {
+                return;
+            }    
+
+            currentTween?.Kill();
+            currentTween = DOTween.Sequence()            
+            .AppendInterval(durationPause)
+            .Append(transform.DOPunchPosition(punchPosition, durationEffect, 10, 3));
+        }
+    }
+}
